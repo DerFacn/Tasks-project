b5d96e1d5ba8ae15f7bd62d428791dd7f2c406b6
themanwhocan <mail981120@gmail.com>
2023-09-29 21:42:51 +0300

    упростил развёртывание базы данных, увеличил область консольной надписи о её инициализации, уменьшил колличество импортируемых функций модуля os

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
+from os.path import abspath, dirname
+from os.path import exists as file_exists
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
-import click
 from flask_jwt_extended import JWTManager
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))  # Database path
+basedir = abspath(dirname(__file__))  # Database path
 app = Flask(__name__)
+
+db_filename = 'app.db'
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['JWT_SECRET_KEY'] = 'secret_key'
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config['JWT_COOKIE_SECURE'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_filename
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -20,14 +23,10 @@ jwt = JWTManager(app)
 from app.models import User, Todo
 
 
-@click.command('create-db')
-def create_db():
+if not file_exists('./instance/' + db_filename):
     with app.app_context():
         db.create_all()
-    click.echo('Database created!')
-
-
-app.cli.add_command(create_db)
+        print('\nDATABASE INITIALIZED\n')
 
 
 from app import auth
