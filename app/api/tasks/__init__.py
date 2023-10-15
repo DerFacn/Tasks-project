f79a0678c19314b44bdd2013f5f2485c21bd14bb
DerFacn <derfacn.pro228@gmail.com>
2023-10-15 16:44:31 +0200

    Tasks endpoints created

--- app/api/tasks/__init__.py
+++ app/api/tasks/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from .routes import create_task, complete_task, delete_task
+from .routes import create_task, get_task, delete_task
 
 from app.misc.config import Config
 
@@ -7,5 +7,5 @@ from app.misc.config import Config
 tasks = Blueprint('tasks', __name__, url_prefix='/tasks', template_folder=Config.TEMPLATE_ABS)
 
 tasks.add_url_rule('/create', 'create', create_task, methods=['POST'])
-tasks.add_url_rule('/complete/<task_id>', 'complete', complete_task, methods=['PUT'])
-tasks.add_url_rule('/delete/<task_id>', 'delete', delete_task, methods=['DELETE'])
+tasks.add_url_rule('/get', 'get', get_task, methods=['GET'])
+tasks.add_url_rule('/delete', 'delete', delete_task, methods=['POST'])
--- app/api/tasks/routes.py
+++ app/api/tasks/routes.py
@@ -1,10 +1,51 @@
-def create_task():  # TODO может уже когда-то тудушники сделать))
-    return ''
+from flask import request
+from app.misc.models import Task
+from app import session
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
-def complete_task():
-    return ''
+@jwt_required()
+def create_task():
+    title = request.form.get('title')
+    text = request.form.get('text')
+    if not title:
+        return {"msg": "Title is required!"}, 422
+    elif not text:
+        return {"msg": "Body text is required!"}, 422
+    user_uuid = get_jwt_identity()
+    new_task = Task(
+        user_uuid=user_uuid,
+        title=title,
+        text=text
+    )
+    session.add(new_task)
+    session.commit()
+    return {"msg": f"Task {title} is created!"}
 
 
+@jwt_required()
+def get_task():
+    user_uuid = get_jwt_identity()
+    offset = request.args.get('offset')
+    limit = request.args.get('limit')
+    query = session.query(Task).filter(Task.user_uuid == user_uuid).offset(offset).limit(limit)
+    tasks = []
+    for task in query:
+        tasks.append({
+            "id": task.id,
+            "title": task.title,
+            "text": task.text
+        })
+    return tasks
+
+
+@jwt_required()
 def delete_task():
-    return ''
+    task_id = request.form.get('task_id')
+    task = session.query(Task).get(task_id)
+    if not task:
+        return {"msg": "Task not found!"}
+    session.delete(task)
+    session.commit()
+    return {"msg": "Task completed!"}
+
