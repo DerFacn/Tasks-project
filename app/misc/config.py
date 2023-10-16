4be392c374d94cc34ebaf8b3c78ec426a7060433
themanwhocan <mail981120@gmail.com>
2023-10-16 20:48:14 +0300

    небольшие изменения

--- app/misc/config.py
+++ app/misc/config.py
@@ -5,4 +5,6 @@ class Config(object):
     JWT_SECRET_KEY = 'jwt_secret_key_here'
     JWT_TOKEN_LOCATION = ['cookies']
     JWT_COOKIE_SECURE = True
+    
+    SEND_FILE_MAX_AGE_DEFAULT = 1
     TEMPLATE_ABS = abspath('../frontend/templates')
